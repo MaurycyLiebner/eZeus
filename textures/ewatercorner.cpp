47e6b6faa4d5a56569525bac5af0ca23eed9fd48
Maurycy Liebner <maurycy.liebner@gmail.com>
2021-07-28 13:05:36 +0200

    Fix some water corners.

--- textures/ewatercorner.cpp
+++ textures/ewatercorner.cpp
@@ -41,9 +41,9 @@ int eWaterCorner::get(eTile* const tile) {
         id = 12;
     } else if(tt(dry, beach, beach | water, water, water, water, water, water)) {
         id = 13;
-    } else if(tt(beach | water, beach, dry, dry, dry, water, water, water)) {
+    } else if(tt(beach | water, beach, dry, dry, dry | water, water, water, water)) {
         id = 14;
-    } else if(tt(dry, beach, beach | water, water, water, water, dry, dry)) {
+    } else if(tt(dry, beach, beach | water, water, water, water, dry | water, dry)) {
         id = 15;
     } else if(tt(beach | water, beach, dry, water, water, water, beach | water, beach)) {
         id = 16;
