c58820f6cb2f71f9a00928d0b5fbec1a33d3d403
Maurycy Liebner <maurycy.liebner@gmail.com>
2021-07-28 12:49:00 +0200

    Additional terrain texture rendering fix.

--- textures/ebeachtodry.cpp
+++ textures/ebeachtodry.cpp
@@ -9,32 +9,34 @@ int eBeachToDry::get(eTile* const tile) {
 
     if(tile->terrain() != beach) return -1;
 
+    const auto bow = beach | eTerrain::water;
+
     eSurroundingTerrain tt(tile);
 
     int id = -1;
-    if(tt(beach, beach, beach, beach, dry | beach, dry, dry | beach, beach)) {
+    if(tt(bow, bow, bow, bow, dry | bow, dry, dry | bow, bow)) {
         id = 0;
-    } else if(tt(beach, beach, beach, beach, beach, beach, dry, beach)) {
+    } else if(tt(bow, bow, bow, bow, bow, bow, dry, bow)) {
         id = 1;
-    } else if(tt(dry | beach, beach, beach, beach, beach, beach, dry | beach, dry)) {
+    } else if(tt(dry | bow, bow, bow, bow, bow, bow, dry | bow, dry)) {
         id = 2;
-    } else if(tt(dry, beach, beach, beach, beach, beach, beach, beach)) {
+    } else if(tt(dry, bow, bow, bow, bow, bow, bow, bow)) {
         id = 3;
-    } else if(tt(dry | beach, dry, dry | beach, beach, beach, beach, beach, beach)) {
+    } else if(tt(dry | bow, dry, dry | bow, bow, bow, bow, bow, bow)) {
         id = 4;
-    } else if(tt(beach, beach, dry, beach, beach, beach, beach, beach)) {
+    } else if(tt(bow, bow, dry, bow, bow, bow, bow, bow)) {
         id = 5;
-    } else if(tt(beach, beach, dry | beach, dry, dry | beach, beach, beach, beach)) {
+    } else if(tt(bow, bow, dry | bow, dry, dry | bow, bow, bow, bow)) {
         id = 6;
-    } else if(tt(beach, beach, beach, beach, dry, beach, beach, beach)) {
+    } else if(tt(bow, bow, bow, bow, dry, bow, bow, bow)) {
         id = 7;
-    } else if(tt(dry | beach, beach, beach, beach, dry | beach, dry, dry | beach, dry)) {
+    } else if(tt(dry | bow, bow, bow, bow, dry | bow, dry, dry | bow, dry)) {
         id = 8;
-    } else if(tt(dry | beach, dry, dry | beach, beach, beach, beach, dry | beach, dry)) {
+    } else if(tt(dry | bow, dry, dry | bow, bow, bow, bow, dry | bow, dry)) {
         id = 9;
-    } else if(tt(dry | beach, dry, dry | beach, dry, dry | beach, beach, beach, beach)) {
+    } else if(tt(dry | bow, dry, dry | bow, dry, dry | bow, bow, bow, bow)) {
         id = 10;
-    } else if(tt(beach, beach, dry | beach, dry, dry | beach, dry, dry | beach, beach)) {
+    } else if(tt(bow, bow, dry | bow, dry, dry | bow, dry, dry | bow, bow)) {
         id = 11;
     }
     return id;
--- textures/ewatercorner.cpp
+++ textures/ewatercorner.cpp
@@ -13,7 +13,7 @@ int eWaterCorner::get(eTile* const tile) {
     eSurroundingTerrain tt(tile);
 
     int id = -1;
-    if(tt(water, water, water, water, beach, beach, dry, water)) {
+    if(tt(water, water, water, water, beach | water, beach, dry, water)) {
         id = 0;
     } else if(tt(water, water, water, water, dry, beach, beach, water)) {
         id = 1;
@@ -21,45 +21,45 @@ int eWaterCorner::get(eTile* const tile) {
         id = 2;
     } else if(tt(water, water, dry, dry, dry, beach, beach, water)) {
         id = 3;
-    } else if(tt(water, water, beach, beach, beach, beach, dry, water)) {
+    } else if(tt(water, water, beach | water, beach, beach | water, beach, dry, water)) {
         id = 4;
-    } else if(tt(beach, water, water, water, dry, beach, beach, beach)) {
+    } else if(tt(beach | water, water, water, water, dry, beach, beach | water, beach)) {
         id = 5;
-    } else if(tt(dry, water, water, water, water, water, beach, beach)) {
+    } else if(tt(dry, water, water, water, water, water, beach | water, beach)) {
         id = 6;
-    } else if(tt(beach, water, water, water, water, water, dry, beach)) {
+    } else if(tt(beach | water, water, water, water, water, water, dry, beach)) {
         id = 7;
-    } else if(tt(dry, dry, dry, water, water, water, beach, beach)) {
+    } else if(tt(dry, dry, dry, water, water, water, beach | water, beach)) {
         id = 8;
-    } else if(tt(beach, water, water, water, dry, dry, dry, beach)) {
+    } else if(tt(beach | water, water, water, water, dry, dry, dry, beach)) {
         id = 9;
-    } else if(tt(dry, water, water, water, beach, beach, beach, beach)) {
+    } else if(tt(dry, water, water, water, beach | water, beach, beach | water, beach)) {
         id = 10;
-    } else if(tt(beach, beach, beach, water, water, water, dry, beach)) {
+    } else if(tt(beach | water, beach, beach | water, water, water, water, dry, beach)) {
         id = 11;
-    } else if(tt(beach, beach, dry, water, water, water, water, water)) {
+    } else if(tt(beach | water, beach, dry, water, water, water, water, water)) {
         id = 12;
-    } else if(tt(dry, beach, beach, water, water, water, water, water)) {
+    } else if(tt(dry, beach, beach | water, water, water, water, water, water)) {
         id = 13;
-    } else if(tt(beach, beach, dry, dry, dry, water, water, water)) {
+    } else if(tt(beach | water, beach, dry, dry, dry, water, water, water)) {
         id = 14;
-    } else if(tt(dry, beach, beach, water, water, water, dry, dry)) {
+    } else if(tt(dry, beach, beach | water, water, water, water, dry, dry)) {
         id = 15;
-    } else if(tt(beach, beach, dry, water, water, water, beach, beach)) {
+    } else if(tt(beach | water, beach, dry, water, water, water, beach | water, beach)) {
         id = 16;
-    } else if(tt(dry, beach, beach, beach, beach, water, water, water)) {
+    } else if(tt(dry, beach, beach | water, beach, beach | water, water, water, water)) {
         id = 17;
-    } else if(tt(water, water, beach, beach, dry, water, water, water)) {
+    } else if(tt(water, water, beach | water, beach, dry, water, water, water)) {
         id = 18;
-    } else if(tt(water, water, dry, beach, beach, water, water, water)) {
+    } else if(tt(water, water, dry, beach, beach | water, water, water, water)) {
         id = 19;
-    } else if(tt(water, water, beach, beach, dry, dry, dry, water)) {
+    } else if(tt(water, water, beach | water, beach, dry, dry, dry, water)) {
         id = 20;
-    } else if(tt(dry, dry, dry, beach, beach, water, water, water)) {
+    } else if(tt(dry, dry, dry, beach, beach | water, water, water, water)) {
         id = 21;
-    } else if(tt(beach, beach, beach, beach, dry, water, water, water)) {
+    } else if(tt(beach | water, beach, beach | water, beach, dry, water, water, water)) {
         id = 22;
-    } else if(tt(water, water, dry, dry, beach, beach, beach, water)) {
+    } else if(tt(water, water, dry, dry, beach | water, beach, beach | water, water)) {
         id = 23;
     }
     return id;
