b3cc12c79af95d8988c43eaa206635d18342323d
Maurycy Liebner <maurycy.liebner@gmail.com>
2021-08-18 14:20:52 +0200

    Some watchman changes.

--- characters/efightingpatroler.cpp
+++ characters/efightingpatroler.cpp
@@ -11,27 +11,28 @@ eFightingPatroler::eFightingPatroler(
 
 }
 
-void eFightingPatroler::setAnimationAction(const eFightingPatrolerAction a) {
-    mAction = a;
-    mActionStartTime = textureTime();
-}
-
 eTexture eFightingPatroler::getTexture(const eTileSize size) const {
     const int id = static_cast<int>(size);
     const auto& charTexs = mTextures[id].*mCharTexs;
     const eTextureCollection* coll = nullptr;
     const int oid = static_cast<int>(orientation());
     bool wrap = true;
-    if(mAction == eFightingPatrolerAction::walk) {
+    const auto a = actionType();
+    switch(a) {
+    case eCharacterActionType::collect:
+    case eCharacterActionType::carry:
+    case eCharacterActionType::walk: {
         coll = &charTexs.fWalk[oid];
-    } else if(mAction == eFightingPatrolerAction::fight) {
+    } break;
+    case eCharacterActionType::fight: {
         coll = &charTexs.fFight[oid];
-    } else if(mAction == eFightingPatrolerAction::die) {
+    } break;
+    case eCharacterActionType::die:
         wrap = false;
         coll = &charTexs.fDie;
     }
     if(!coll || coll->size() == 0) return eTexture();
-    const int t = textureTime() - mActionStartTime;
+    const int t = textureTime() - actionStartTime();
     int texId = t % coll->size();
     if(!wrap) texId = std::clamp(texId, 0, coll->size() - 1);
     return coll->getTexture(texId);
--- characters/efightingpatroler.h
+++ characters/efightingpatroler.h
@@ -5,24 +5,14 @@
 
 #include "textures/echaractertextures.h"
 
-enum class eFightingPatrolerAction {
-    walk, fight, die
-};
-
 class eFightingPatroler : public eCharacter {
 public:
     using eCharTexs = eFightingPatrolerTextures eCharacterTextures::*;
     eFightingPatroler(eGameBoard& board, const eCharTexs charTexs,
                       const eCharacterType type);
 
-    eFightingPatrolerAction animationAction() const { return mAction; }
-    void setAnimationAction(const eFightingPatrolerAction a);
-
     eTexture getTexture(const eTileSize size) const;
 private:
-    int mActionStartTime = 0;
-    eFightingPatrolerAction mAction{eFightingPatrolerAction::walk};
-
     const std::vector<eCharacterTextures>& mTextures;
     const eCharTexs mCharTexs;
 };
