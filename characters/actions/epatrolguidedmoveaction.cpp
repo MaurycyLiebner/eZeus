4a15d7880c27b2febb5545fd6d1d9013522e3619
maurycyliebner <maurycy.liebner@gmail.com>
2021-09-19 18:47:43 +0200

    ePatrolGuidedMoveAction -> eMoveToAction.

--- characters/actions/epatrolguidedmoveaction.cpp
+++ characters/actions/epatrolguidedmoveaction.cpp
@@ -1,11 +1,7 @@
 #include "epatrolguidedmoveaction.h"
 
-#include "emovepathaction.h"
+#include "emovetoaction.h"
 #include "../echaracter.h"
-#include "engine/egameboard.h"
-#include "engine/ethreadpool.h"
-#include "epathfindtask.h"
-#include "characters/actions/ewaitaction.h"
 
 ePatrolGuidedMoveAction::ePatrolGuidedMoveAction(
         eCharacter* const c,
@@ -31,16 +27,7 @@ void ePatrolGuidedMoveAction::nextGuide() {
     const auto& g = mGuides.at(mNextGuide++);
 
     const auto c = character();
-    const auto t = c->tile();
-    const auto& brd = c->getBoard();
-    const auto tp = brd.threadPool();
 
-    const int tx = t->x();
-    const int ty = t->y();
-
-    const auto startTile = [tx, ty](eThreadBoard& board) {
-        return board.absTile(tx, ty);
-    };
     const auto finalTile = [g](eThreadTile* const t) {
         return t->x() == g.fX && t->y() == g.fY;
     };
@@ -49,32 +36,12 @@ void ePatrolGuidedMoveAction::nextGuide() {
         if(tptr) tptr->setState(eCharacterActionState::failed);
     };
 
-    const auto rect = mBuildingRect;
-    const auto walkable = [rect](eTileBase* const t) {
-        const SDL_Point p{t->x(), t->y()};
-        const bool r = SDL_PointInRect(&p, &rect);
-        if(r) return true;
-        return t->hasRoad();
+    const auto finishAction = [tptr]() {
+        if(tptr) tptr->nextGuide();
     };
 
-    const auto finishFunc = [tptr, c, walkable, failFunc](
-                            const std::vector<eOrientation>& path) {
-        if(!tptr) return;
-        const auto finishAction = [tptr]() {
-            if(tptr) tptr->nextGuide();
-        };
-
-        const auto a  = e::make_shared<eMovePathAction>(
-                            c, path, walkable,
-                            failFunc, finishAction);
-        tptr->setCurrentAction(a);
-    };
-
-    const auto pft = new ePathFindTask(startTile, walkable,
-                                       finalTile, finishFunc,
-                                       failFunc, false, 50);
-    tp->queueTask(pft);
-
-    setCurrentAction(e::make_shared<eWaitAction>(c, []() {}, []() {}));
-
+    const auto a  = e::make_shared<eMoveToAction>(
+                        c, failFunc, finishAction);
+    a->start(finalTile, eMoveToAction::sRoadWalkable);
+    tptr->setCurrentAction(a);
 }
