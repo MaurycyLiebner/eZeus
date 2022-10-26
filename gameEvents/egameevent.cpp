1a006d50f961e5ed90591787b7a60a167b0715ba
maurycyliebner <maurycy.liebner@gmail.com>
2022-10-26 11:40:53 +0200

    Add eGodAttackEvent load creation.

--- gameEvents/egameevent.cpp
+++ gameEvents/egameevent.cpp
@@ -1,6 +1,7 @@
 #include "egameevent.h"
 
 #include "egodvisitevent.h"
+#include "egodattackevent.h"
 
 eGameEvent::eGameEvent(const eGameEventType type,
                        eGameBoard& board) :
@@ -11,5 +12,7 @@ stdsptr<eGameEvent> eGameEvent::sCreate(const eGameEventType type,
     switch(type) {
     case eGameEventType::godVisit:
         return e::make_shared<eGodVisitEvent>(board);
+    case eGameEventType::godAttack:
+        return e::make_shared<eGodAttackEvent>(board);
     }
 }
