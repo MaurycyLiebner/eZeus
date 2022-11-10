eb84c2ec7facd1ec2728388310b759f10cb91b36
maurycyliebner <maurycy.liebner@gmail.com>
2022-11-10 18:25:41 +0100

    Fix granary overlay positions.

--- buildings/egranary.cpp
+++ buildings/egranary.cpp
@@ -21,17 +21,17 @@ std::vector<eOverlay> eGranary::getOverlays(const eTileSize size) const {
     const int texId = textureTime() % coll.size();
     auto& o = os.emplace_back();
     o.fTex = coll.getTexture(texId);
-    o.fX = 0.5;
-    o.fY = -3.81;
+    o.fX = 0.38;
+    o.fY = -3.78;
     const std::pair<double, double> xy[8] = {{-3.11, -5.1},
-                                             {-3, -5.7},
-                                             {-2.89, -6.3},
+                                             {-3, -5.65},
+                                             {-2.89, -6.25},
 
                                              {-2.64, -4.43},
-                                             {-2.53, -5.03},
-                                             {-2.42, -5.63},
-                                             {-2.31, -6.23},
-                                             {-2.20, -6.83}};
+                                             {-2.53, -5.00},
+                                             {-2.42, -5.55},
+                                             {-2.31, -6.13},
+                                             {-2.20, -6.73}};
     for(int i = 0; i < 8; i++) {
         const int count = resourceCount(i);
         if(count <= 0) continue;
