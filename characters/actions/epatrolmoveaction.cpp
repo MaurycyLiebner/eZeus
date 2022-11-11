fab489172d48f9678d54abd13b648a9010fb7774
maurycyliebner <maurycy.liebner@gmail.com>
2022-11-11 14:09:11 +0100

    Defend against patrol looping.

--- characters/actions/epatrolmoveaction.cpp
+++ characters/actions/epatrolmoveaction.cpp
@@ -33,13 +33,32 @@ eCharacterActionState ePatrolMoveAction::nextTurn(eOrientation& t) {
     if(options.empty()) {
         mO = !mO;
     } else {
-        const int rid = rand() % options.size();
-        mO = options[rid].first;
+        auto& uses = mOs[tile];
+        int min = __INT_MAX__;
+        std::vector<eOrientation> minOs;
+        minOs.reserve(options.size());
+        for(const auto& o : options) {
+            const auto oo = o.first;
+            int& u = uses.usage(oo);
+            if(u < min) {
+                minOs.clear();
+                minOs.push_back(oo);
+                min = u;
+            } else if(u == min) {
+                minOs.push_back(oo);
+            }
+        }
+        if(!minOs.empty()) {
+            const int id = rand() % minOs.size();
+            mO = minOs[id];
+        }
     }
     t = mO;
-    const auto tt = tile->neighbour(mO);
+    const auto tt = tile->neighbour<eTile>(mO);
     if(!tt) {
         return eCharacterActionState::failed;
     }
+    mOs[tile].usage(mO)++;
+    mOs[tt].usage(!mO)++;
     return eCharacterActionState::running;
 }
--- characters/actions/epatrolmoveaction.h
+++ characters/actions/epatrolmoveaction.h
@@ -11,6 +11,22 @@ class eMovePathAction;
 
 class eBuilding;
 
+class eDirectionUsage {
+public:
+    eDirectionUsage() {
+        for(int i = 0; i < 8; i++) {
+            mUsage.push_back(0);
+        }
+    }
+
+    int& usage(const eOrientation o) {
+        const int id = static_cast<int>(o);
+        return mUsage[id];
+    }
+private:
+    std::vector<int> mUsage;
+};
+
 class ePatrolMoveAction : public eMoveAction {
 public:
     using eTileWalkable = std::function<bool(eTileBase* const)>;
@@ -32,6 +48,8 @@ private:
     eOrientation mO{eOrientation::topRight};
     int mMaxWalkDistance = 10000;
     int mWalkedDistance = 0;
+
+    std::map<eTile*, eDirectionUsage> mOs;
 };
 
 #endif // EPATROLMOVEACTION_H
