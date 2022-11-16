ba758e9a523bb0d4422d9549ff581e9b1f0e665c
maurycyliebner <maurycy.liebner@gmail.com>
2022-11-16 16:17:20 +0100

    Use eFramedButtons for eGameMainMenu.

--- widgets/egamemainmenu.cpp
+++ widgets/egamemainmenu.cpp
@@ -1,6 +1,6 @@
 #include "egamemainmenu.h"
 
-#include "ebutton.h"
+#include "eframedbutton.h"
 
 #include "elanguage.h"
 
@@ -13,28 +13,32 @@ void eGameMainMenu::initialize(const eAction& resumeAct,
                                const eAction& exitAct) {
     setType(eFrameType::message);
 
-    const auto resButt = new eButton(window());
+    const auto resButt = new eFramedButton(window());
+    resButt->setUnderline(false);
     resButt->setText(eLanguage::text("resume_game"));
     resButt->fitContent();
     resButt->setPressAction(resumeAct);
     addWidget(resButt);
     resButt->align(eAlignment::hcenter);
 
-    const auto saveButt = new eButton(window());
+    const auto saveButt = new eFramedButton(window());
+    saveButt->setUnderline(false);
     saveButt->setText(eLanguage::text("save_game"));
     saveButt->fitContent();
     saveButt->setPressAction(saveAct);
     addWidget(saveButt);
     saveButt->align(eAlignment::hcenter);
 
-    const auto loadButt = new eButton(window());
+    const auto loadButt = new eFramedButton(window());
+    loadButt->setUnderline(false);
     loadButt->setText(eLanguage::text("load_game"));
     loadButt->fitContent();
     loadButt->setPressAction(loadAct);
     addWidget(loadButt);
     loadButt->align(eAlignment::hcenter);
 
-    const auto exitButt = new eButton(window());
+    const auto exitButt = new eFramedButton(window());
+    exitButt->setUnderline(false);
     exitButt->setText(eLanguage::text("exit"));
     exitButt->fitContent();
     exitButt->setPressAction(exitAct);
